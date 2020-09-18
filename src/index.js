b8827d31547f8155ac8c29c27b729b6cb5f0df96
sriv <srikanth.ddit@gmail.com>
2020-09-18 15:46:24 +0530

    remove wrong license header
    Signed-off-by: sriv <srikanth.ddit@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-/*----------------------------------------------------------------
- *  Copyright (c) ThoughtWorks, Inc.
- *  Licensed under the Apache License, Version 2.0
- *  See LICENSE in the project root for license information.
- *----------------------------------------------------------------*/
-
 const core = require("@actions/core"),
   github = require("@actions/github"),
   Adm = require("adm-zip"),
